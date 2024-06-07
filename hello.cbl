82fb1ece7f54df9d8e9fbc568acf615189427935
mhashim1988 <148561645+mhashim1988@users.noreply.github.com>
2024-06-07 09:19:40 -0400

    Create hello.cbl

---
+++ hello.cbl
@@ -0,0 +1,9 @@
+   ID DIVISION.
+       PROGRAM-ID. HELLO.
+
+      *    This is a Demo Program
+      *
+      *    THIS PROGRAM WILL RECEIVE A DATE AND COVERT THE DATE TO
+      *    AN INTEGER IN A CALLED PROGRAM TO DETERMINE DAYS FROM
+      *    CURRENT DATE.
+      * TRAINING PUPOSE
